9b9d406a8836c77f7d535040020c348688f80366
alefadonais5 <alefadonais5@gmail.com>
2024-07-13 12:51:57 -0300

    Calculadora

---
+++ script.js
@@ -0,0 +1,8 @@
+function displaynum(n1){
+    Calculador.text1.value = Calculador.text1.value + n1
+}
+
+
+// function displaynum(n1) {
+//     document.forms['Calculator'].elements['text1'].value += n1;
+// }
